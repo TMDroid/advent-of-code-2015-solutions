89ca5918ed23ae03815169aa84137bea556a9d29
Daniel Bejan <dannyb97danny@gmail.com>
2018-12-09 03:22:01 +0200

    Day 2 - Part 2

--- 2/main.py
+++ 2/main.py
@@ -1,18 +1,30 @@
+from functools import reduce
+
 with open("input") as f:
     content = f.readlines()
 
 total = 0
+ribbonTotal = 0
 for dimensions in content:
-    dimensions = dimensions.split("x")
-    l = int(dimensions[0])
-    w = int(dimensions[1])
-    h = int(dimensions[2])
+    d = list(map(lambda x: int(x), dimensions.split("x")))
 
-    sides = [l * w, w * h, l * h]
+    #1
+    sides = [d[0] * d[1], d[1] * d[2], d[0] * d[2]]
     sides.append(min(sides))
     for side in range(0, len(sides) - 1):
         sides[side] *= 2
 
     total += sum(sides)
 
-print(total)
+    #2
+    biggest = max(d)
+    d.remove(biggest)
+    ribbon = reduce(lambda x, y: x + x + y + y, d)
+    d.append(biggest)
+    bowtie = reduce(lambda x, y: x * y, d)
+
+    ribbonTotal += ribbon + bowtie
+
+
+print("square feet of wrapping paper: {0}".format(total))
+print("feet of ribbon : {0}".format(ribbonTotal))

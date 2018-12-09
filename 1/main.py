c13d963dc531bdec5310be84872342a9fe513177
Daniel Bejan <dannyb97danny@gmail.com>
2018-12-09 02:55:14 +0200

    Day 1 - Part 2

--- 1/main.py
+++ 1/main.py
@@ -2,7 +2,10 @@ with open("input") as f:
     content = f.readlines()[0]
 
 currentFloor = 0
+currentIndex = 0
 for c in content:
     currentFloor += (1 if c == '(' else -1)
-
-print(currentFloor)
+    currentIndex += 1
+    if currentFloor == -1:
+        print("Floor is -1: {0}".format(currentIndex))
+        break
